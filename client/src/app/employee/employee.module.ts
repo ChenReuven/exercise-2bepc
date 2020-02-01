2c20b7f2e1df77a9fedd5997608674c906ac2083
Chen Reuven <Chen.Reuven@emc.com>
2020-02-01 20:47:14 +0200

    implement employees page

--- client/src/app/employee/employee.module.ts
+++ client/src/app/employee/employee.module.ts
@@ -1,12 +1,16 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {EmployeeComponent} from './pages/employee.component';
+import {MatListModule} from "@angular/material/list";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [EmployeeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatListModule,
+    MatButtonModule
   ]
 })
 export class EmployeeModule { }
